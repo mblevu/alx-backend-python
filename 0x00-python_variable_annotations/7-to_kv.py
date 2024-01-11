d0d6106628ab2cdd2ca2eb3312fc568c30f34de2
mblevu <brianislevu@gmail.com>
2024-01-12 02:03:34 +0300

    fixing kv error

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """return int/float tuple to_kv"""
-from typing import List, Union, Tuple
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: [Union[int, float]]) -> Tuple[str, float]:
