92586188268a22bd431777cdfe8ed478526df9bd
mblevu <brianislevu@gmail.com>
2024-01-12 02:05:32 +0300

    fixing sum list type

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+from typing import List
 """takes a list input_list of floats as argument
 and returns their sum as a float."""
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """return sum of list of floats"""
     return sum(input_list)
