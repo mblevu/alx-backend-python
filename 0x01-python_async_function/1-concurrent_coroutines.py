3de3e3df79a944c68dafcd6c5cb31cd64f55af3c
mblevu <brianislevu@gmail.com>
2024-01-16 23:10:14 -0800

    Add shebang and import statements

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """"""
+    """spawn wait_random n times with the specified max_delay"""
     list_float = []
     for _ in range(n):
         list_float.append(asyncio.create_task(wait_random(max_delay)))
