b0d5a70fad3c8ccba6855dd23318c65c822238b7
mblevu <brianislevu@gmail.com>
2024-01-16 23:14:37 -0800

    Refactor task_wait_n function to use type hints and add docstrings

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
+"""Take the code from wait_n and alter it into a new function task_wait_n"""
+import asyncio
+import random
+from typing import List
 import asyncio
 import random
 from typing import List
 
 
 async def task_wait_random(max_delay: int) -> int:
-    delay = random.randint(0, max_delay)
+    """takes an integer max_delay and returns a asyncio.Task."""
+    delay: int = random.randint(0, max_delay)
     await asyncio.sleep(delay)
     return delay
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[int]:
-    delay_list = []
+    """alter it into a new function task_wait_n"""
+    delay_list: List[int] = []
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     for task in asyncio.as_completed(tasks):
-        delay = await task
+        delay: int = await task
         delay_list.append(delay)
     return delay_list
