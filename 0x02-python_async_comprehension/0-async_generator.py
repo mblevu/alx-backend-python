b5eb9beebfd9266784bead1ff24b036380b473f8
mblevu <brianislevu@gmail.com>
2024-01-16 22:47:26 -0800

    asynchronously yield random numbers

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""coroutine that takes no arugmensts"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """loop 10 times each async wait 1 second then
+    yield random number btn 0 and 10"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
